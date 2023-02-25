faf086f349992e48ca3d9d77a9830954c7b277b5
penguinDQ_247@outlook.com <penguinDQ_247@outlook.com>
2023-02-25 10:49:28 +0700

    dq-25/2

---
+++ Tourist_VietripInsum_2023/Controllers/LoginStaffController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tourist_VietripInsum_2023.Controllers
+{
+    public class LoginStaffController : Controller
+    {
+        // GET: LoginStaff
+        public ActionResult LoginStaff()
+        {
+            return View();
+        }
+    }
+}
