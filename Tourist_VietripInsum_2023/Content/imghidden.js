e6a7f068cdb244cff0f14e6e4125eadf91aef4b9
penguinDQ_247@outlook.com <penguinDQ_247@outlook.com>
2023-03-15 23:00:43 +0700

    dq-guest-homepage-ss1

--- Tourist_VietripInsum_2023/Content/imghidden.js
+++
@@ -1,7 +0,0 @@
-﻿ImagerTour.onchange = evt => {
-    const [file] = ImagerTour.files
-    if (file) {
-        preview.src = URL.createObjectURL(file);
-        document.getElementById("preview").style.visibility = "visible";
-    }
-}
