3d3e14d417959677c1c3919853c759d70aef6420
penguinDQ_247@outlook.com <penguinDQ_247@outlook.com>
2023-03-20 23:05:56 +0700

    dq-update-guest homepage

--- Tourist_VietripInsum_2023/Controllers/GuestController.cs
+++ Tourist_VietripInsum_2023/Controllers/GuestController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tourist_VietripInsum_2023.Models;
 
 namespace Tourist_VietripInsum_2023.Controllers
 {
     public class GuestController : Controller
     {
+        TouristEntities1 db = new TouristEntities1();
         // GET: Guest
         public ActionResult HomePageGuest()
         {
             return View();
+        }
+
+        public ActionResult Tourinfomation(string id)
+        {
+            return View(db.Tours.Where(s => s.MaLTour == id).FirstOrDefault());
         }
     }
 }
