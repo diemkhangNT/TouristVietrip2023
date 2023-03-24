9f6135d0b91b2a57e6135c94413bb137e49acd08
penguinDQ_247@outlook.com <penguinDQ_247@outlook.com>
2023-03-24 10:27:00 +0700

    dq-listtour 24/3-10h30

--- Tourist_VietripInsum_2023/Controllers/GuestController.cs
+++ Tourist_VietripInsum_2023/Controllers/GuestController.cs
@@ -43,5 +43,58 @@ namespace Tourist_VietripInsum_2023.Controllers
             }
             return View(tour);
         }
+
+        //List tour tinh thanh
+
+        public List<Tour> Laytour(string id)
+        {
+            var tinhthanh = db.DiaDiemThamQuans.Where(t => t.MaTinh == id).FirstOrDefault();
+
+            List<ChiTietTour> ct = db.ChiTietTours.Where(t => t.MaDDTQ == tinhthanh.MaDDTQ).ToList();
+
+            List<Tour> listtour = new List<Tour>();
+            foreach (var item in ct)
+            {
+                if(item.STT==1)
+                {
+                    var t = db.Tours.Where(m => m.MaTour == item.MaTour).FirstOrDefault();
+                    listtour.Add(t);
+                }    
+            }
+            return listtour;
+        }
+
+        public List<Tour> Lay_tourtheo(string id)
+        {
+            List<Hotel> hotels = db.Hotels.Where(t => t.MaTinh == id).ToList();
+            var tinhthanh = db.TinhThanhs.Where(t => t.MaTinh == id).FirstOrDefault();
+            Session["tentinh"] = tinhthanh.TenTinh;
+            List<Tour> listtour = new List<Tour>();
+            foreach (var item in hotels)
+            {
+
+                var t = db.Tours.Where(m => m.MaKS == item.MaKS).FirstOrDefault();
+                listtour.Add(t);
+
+            }
+            return listtour;
+        }
+        public ActionResult ListTour(string id)
+        {
+            //var tinhthanh = db.DiaDiemThamQuans.Where(t => t.MaTinh == id).FirstOrDefault();
+
+            var listtourTinh = Lay_tourtheo(id);
+            return View(listtourTinh);
+        }
+        //dia danh
+
+        public ActionResult List_Tour(string id)
+        {
+            var tinhthanh = db.DiaDiemThamQuans.Where(t => t.MaDDTQ == id).FirstOrDefault();
+            var listtourDD = Lay_tourtheo(tinhthanh.MaTinh);
+            return View(listtourDD);
+        }
+
+
     }
-}
+    }
